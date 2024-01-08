5003c9ad5ec9da14c371cb18bb1987cd185800ab
Dame <demechelsea@gmail.com>
2024-01-08 15:13:23 +0300

    portifolio using react

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,24 @@
+import Image1 from "./../../assets/abdi.JPEG"
+import Image2 from "./../../assets/ephrem1.png"
+import Image3 from "./../../assets/debby.png"
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "Abdi Fekadu",
+        description: "She knows everything "
+    },
+    {
+        id: 1,
+        image: Image2,
+        title: "Ephrem Daniel",
+        description: "They knows everything"
+    },
+    {
+        id: 1,
+        image: Image3,
+        title: "Debi Miressa",
+        description: "I know everything"
+    },
+]
